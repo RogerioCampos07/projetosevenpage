13960ed8322c9e3adeba965273ee546d96649dc8
Rogerio Campos <rogerio.campos07@gmail.com>
2026-03-31 22:50:26 -0300

    config: config admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Category, Language, Framework, Project
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Framework)
+class FrameworkAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    # 1. Adicionamos a visualização da imagem na lista
+    list_display = ('get_thumbnail', 'title', 'category', 'created_at')
+    list_filter = ('category', 'languages', 'frameworks', 'created_at')
+    search_fields = ('title', 'description')
+    filter_horizontal = ('languages', 'frameworks')
+    
+    # 2. Navegação rápida por data
+    date_hierarchy = 'created_at'
+    
+    # 3. Organização Visual dos Campos (Fieldsets)
+    fieldsets = (
+        ('Informações Básicas', {
+            'fields': ('title', 'category', 'description', 'thumbnail')
+        }),
+        ('Tecnologias', {
+            'fields': ('languages', 'frameworks'),
+            'description': 'Selecione as linguagens e frameworks utilizados.'
+        }),
+        ('Links e Demonstração', {
+            'classes': ('collapse',), # Essa seção começa escondida (mais limpo!)
+            'fields': ('github_link', 'youtube_link', 'live_demo'),
+        }),
+    )
+
+    # Função para exibir a imagem na listagem do Admin
+    def get_thumbnail(self, obj):
+        if obj.thumbnail:
+            return format_html('<img src="{}" style="width: 50px; height: auto; border-radius: 5px;" />', obj.thumbnail.url)
+        return "Sem imagem"
+    get_thumbnail.short_description = 'Capa'
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self): return self.name
+
+class Language(models.Model):  # A novidade do elenco!
+    name = models.CharField(max_length=100)
+    def __str__(self): return self.name
+
+class Framework(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self): return self.name
+
+class Project(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='projects')
+    languages = models.ManyToManyField(Language, blank=True)   # Relação N-N
+    frameworks = models.ManyToManyField(Framework, blank=True) # Relação N-N
+    
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    thumbnail = models.ImageField(upload_to='projects/thumbnails/')
+    
+    github_link = models.URLField(max_length=200, null=True, blank=True)
+    youtube_link = models.URLField(max_length=200, null=True, blank=True)
+    live_demo = models.URLField(blank=True, null=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.category.name}"
